3022720a7f72bf656ec755c343b50bb6a1e1797f
Dorian <dorian@latouche.dev>
2022-11-29 15:45:04 +0100

    initialize package

---
+++ src/components/backdrop/backdrop.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+/**
+ * Backdrop is used for dialog, dropdown menu, popover...
+ */
+
+export interface BackdropProps {
+	/** React element displayed in the backdrop. */
+	children?: React.ReactElement;
+	/** ClassName passed to the backdrop, can be used to change the background color for example. */
+	className?: string;
+	/** Boolean to show or hide the backdrop. */
+	open: boolean;
+	/** Calback when the backdrop was clicked */
+	onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export const Backdrop = ({ children, className, open, onClick }: BackdropProps) => {
+	const classNameDefault = `fixed inset-0 overflow-hidden `;
+	const classNameOpen = `${classNameDefault} ${className} z-50 opacity-100 dialogTransitionOpen`;
+	const classNameClose = `${classNameDefault} ${className} -z-50 opacity-0 dialogTransitionClose`;
+	return (
+		<div onClick={onClick} className={open ? classNameOpen : classNameClose}>
+			{children}
+		</div>
+	);
+};
---
+++ src/components/svg/left-svg.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const LeftSvg = (props: SVGProps<SVGSVGElement>) => (
+	<svg viewBox="0 0 24 24" width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<path d="m15 18-6-6 6-6" />
+	</svg>
+);
+
+export default LeftSvg;
